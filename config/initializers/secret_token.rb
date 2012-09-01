a8b81e77e2a92fef4ec130cf00946e1e9b1bd843
Kyle Gann <kylegann@gmail.com>
2012-09-01 00:37:04 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tryagann::Application.config.secret_token = '1b476e1e58b198c776ebd44469bf6c2819d882a2921e0598bebd0cc3fd1810f6495e6a887601a98a5c758e85ea41070382584caf5b6a96a33400fa4d3829c075'
